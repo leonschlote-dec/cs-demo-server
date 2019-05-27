7bfd64930e90c2a482f10c4cd66c173e0893b3cb
Leon Schlote <leonschlote@gmail.com>
2019-05-27 10:10:37 +0200

    new code for upload files

--- public/upload_files.ps1
+++ public/upload_files.ps1
@@ -1,18 +1,22 @@
-$server = "raspberrypi.local"
-#$dir = "C:\Users\testo\Desktop\*.*"
-$dir = "C:/Users/testo/Desktop"
-$user = "ftpuser"
-$password = "ftppass"
+# Config
+$Username = "ftpuser"
+$Password = "ftppass"
+$LocalFile = "C:\Users\testo\geheim.txt"
+$RemoteFile = "ftp://raspberrypi.local/TEST/files/geheim.txt"
 
-
-#$filelist = [string[]] (Get-ChildItem -Path $dir -Recurse)
-$filelist = "geheim.txt "
-
-#echo $filelist
-
-"open $server
-user $user $password
-binary
-cd $dir
-" +
-($filelist.split(' ') | %{ "put ""$_""`n" }) | ftp -i -in
+# Create FTP Rquest Object
+$FTPRequest = [System.Net.FtpWebRequest]::Create("$RemoteFile")
+$FTPRequest = [System.Net.FtpWebRequest]$FTPRequest
+$FTPRequest.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$FTPRequest.Credentials = new-object System.Net.NetworkCredential($Username, $Password)
+$FTPRequest.UseBinary = $true
+$FTPRequest.UsePassive = $true
+# Read the File for Upload
+$FileContent = gc -en byte $LocalFile
+$FTPRequest.ContentLength = $FileContent.Length
+# Get Stream Request by bytes
+$Run = $FTPRequest.GetRequestStream()
+$Run.Write($FileContent, 0, $FileContent.Length)
+# Cleanup
+$Run.Close()
+$Run.Dispose()

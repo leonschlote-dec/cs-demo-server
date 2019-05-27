6e430fc7953a281014714f97e0dca3b7964dcc0f
Leon Schlote <leonschlote@gmail.com>
2019-05-27 10:01:51 +0200

    gfagsdf

--- public/upload_files.ps1
+++ public/upload_files.ps1
@@ -1,11 +1,9 @@
 $server = "raspberrypi.local"
 #$dir = "C:\Users\testo\Desktop\*.*"
-$dir = "C:\Users\testo\Desktop"
+$dir = "C:/Users/testo/Desktop"
 $user = "ftpuser"
 $password = "ftppass"
 
-
-#gci $path | select -expand FullName
 
 #$filelist = [string[]] (Get-ChildItem -Path $dir -Recurse)
 $filelist = "geheim.txt "
